5ef8d497d2f3a68e163fe23fe261a59f0846e823
Nikolai <kolya_cypandin@mail.ru>
2022-06-17 04:31:36 +0300

    not build dir

--- gitpckg.sh
+++ gitpckg.sh
@@ -3,7 +3,7 @@
 function prepare_commits() {
   N=$1
   ((N = N + 1))
-  git stash save build/mispi-3.jar
+  git stash save -u build/mispi-3.jar
   for ((counter = 2; counter <= $N; counter++)); do
     ARG="$counter""p"
     COMMIT=$(git log | grep commit | head -$N | awk '{print $2}' | sed -n $ARG)
@@ -14,10 +14,10 @@ function prepare_commits() {
     ant build
 
     ((NUMBER = counter - 1))
-    CUR_JAR=build/commit$NUMBER.jar
+    CUR_JAR=commits/commit$NUMBER.jar
     mv build/mispi-3.jar $CUR_JAR
 
-    git stash save $CUR_JAR
+    git stash save -u $CUR_JAR
   done
   git checkout master
   for ((i = 2; i <= $N; i++)); do
