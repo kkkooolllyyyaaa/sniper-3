f11f507993a42819742dc431a300b6df7a9e398a
Nikolai <kolya_cypandin@mail.ru>
2022-06-17 04:28:36 +0300

    fixed

--- gitpckg.sh
+++ gitpckg.sh
@@ -3,22 +3,21 @@
 function prepare_commits() {
   N=$1
   ((N = N + 1))
-  git stash push -a build/mispi-3.jar
+  git stash save build/mispi-3.jar
   for ((counter = 2; counter <= $N; counter++)); do
     ARG="$counter""p"
     COMMIT=$(git log | grep commit | head -$N | awk '{print $2}' | sed -n $ARG)
-    echo commit$counter=$COMMIT >>'commits.properties'
 
     git checkout $COMMIT
     git checkout master build.xml
-    git checkout master temp.properties
+    git checkout master temps.properties
     ant build
 
     ((NUMBER = counter - 1))
     CUR_JAR=build/commit$NUMBER.jar
     mv build/mispi-3.jar $CUR_JAR
 
-    git stash push -a $CUR_JAR
+    git stash save $CUR_JAR
   done
   git checkout master
   for ((i = 2; i <= $N; i++)); do
