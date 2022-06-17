bca53613290320dc1247dc10655b0ea8850d99fe
Nikolai <kolya_cypandin@mail.ru>
2022-06-17 04:35:40 +0300

    u key used for save

--- gitpckg.sh
+++ gitpckg.sh
@@ -3,7 +3,7 @@
 function prepare_commits() {
   N=$1
   ((N = N + 1))
-  git stash save -u build/mispi-3.jar
+  #  git stash save -u build/mispi-3.jar
   for ((counter = 2; counter <= $N; counter++)); do
     ARG="$counter""p"
     COMMIT=$(git log | grep commit | head -$N | awk '{print $2}' | sed -n $ARG)
@@ -23,9 +23,9 @@ function prepare_commits() {
   for ((i = 2; i <= $N; i++)); do
     git stash pop
   done
-    zip build/gitpackage.zip build/commit*
-    git stash pop
-    rm -rf build/commit*
+  zip build/gitpackage.zip build/commit*
+  git stash pop
+  rm -rf build/commit*
 }
 
 prepare_commits $1
